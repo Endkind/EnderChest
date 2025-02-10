e987b414510b678ebd92d30f6ad469fad5e1ee70
Endkind <endkind.ender@gmail.com>
2025-02-10 22:54:56 +0100

    - Remove unused import - instance get now saved

--- src/main/java/net/endkind/enderChest/EnderChest.java
+++ src/main/java/net/endkind/enderChest/EnderChest.java
@@ -2,13 +2,13 @@ package net.endkind.enderChest;
 
 import net.endkind.enderChest.Command.OpenEnderChest;
 import net.endkind.enderCore.platform.papermc.EnderPlugin;
-import org.bukkit.command.CommandExecutor;
 
-public final class EnderChest extends EnderPlugin implements CommandExecutor {
+public final class EnderChest extends EnderPlugin {
     private static EnderChest instance;
 
     @Override
     public void onPluginEnable() {
+        instance = this;
         registerBukkitCommand("ec", new OpenEnderChest());
     }
 
